2e393ecc3def7b4cca74182393dd76db2c638ac6
Akshay Pai <akshaypaik@gmail.com>
2025-03-12 22:39:29 +0530

    array reduce method and restructuring the repo

--- Array Methods/array-reduce.js
+++ Array Methods/array-reduce.js
@@ -6,3 +6,16 @@ const sum = numbers.reduce((total, numbers) => {
 
 console.log("numbers: ", numbers);
 console.log("total sum: ", sum);
+
+
+// **************************************************************************************************************
+
+const deposits = [250, 500, 340, 750, 1500, 2100, 3600, 800];
+// I want how many deposits are more than 1000 rs
+// we can achieve this using reduce.
+const numDepositThousand = deposits.reduce((acc, sum) => {
+    acc = sum >= 1000 ? acc + 1 : acc;
+    return acc;
+}, 0); 
+console.log("numDepositThousand: ", numDepositThousand);    // thats 3
+
--- IncrementsConsoleLogCode.js
+++ IncrementsConsoleLogCode.js
@@ -1,5 +1,5 @@
 var a = 5;
-console.log("a++: ", a++);
+console.log("a++: ", a++);  // still returns the previous value that is 5
 console.log("a: ", a);
 console.log("++a: ", ++a);
 console.log("a: ", a);
