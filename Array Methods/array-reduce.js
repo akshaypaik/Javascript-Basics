4900b05e7b7bde123ef9a6b64d00c741b0fc43e8
Akshay Pai <akshaypaik@gmail.com>
2025-03-13 14:32:25 +0530

    advanced reduce array method

--- Array Methods/array-reduce.js
+++ Array Methods/array-reduce.js
@@ -10,12 +10,20 @@ console.log("total sum: ", sum);
 
 // **************************************************************************************************************
 
-const deposits = [250, 500, 340, 750, 1500, 2100, 3600, 800];
+const bankTransactions = [250, 500, 340, 750, -800, 1500, 2100, 3600, 800, -150, -750, -360];
 // I want how many deposits are more than 1000 rs
 // we can achieve this using reduce.
-const numDepositThousand = deposits.reduce((acc, sum) => {
+const numDepositThousand = bankTransactions.reduce((acc, sum) => {
     acc = sum >= 1000 ? acc + 1 : acc;
     return acc;
 }, 0); 
 console.log("numDepositThousand: ", numDepositThousand);    // thats 3
+
+// I want to get all the deposit and withdraw count 
+// acc.withdrawals += sum; is a shorthand of acc.withdrawals = acc.withdrawals + sum;
+const sums = bankTransactions.reduce((acc, sum) => {
+    sum > 0 ? acc.deposits = acc.deposits + sum : acc.withdrawals += sum;
+    return acc;
+}, { deposits: 0, withdrawals: 0 });
+console.log(`Amount of deposits is ${sums.deposits} and withdrawals is ${sums.withdrawals}`);
 
